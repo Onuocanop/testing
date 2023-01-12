2d654eb1403cd7197c19fe0b66915f7bea64d33a
Onuocanop <75081984+Onuocanop@users.noreply.github.com>
2023-01-12 10:31:20 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
-public class test{
-  public static void main(){
-    System.out.println("Please, don't look! This is only a test");
-  }
+public class Hello {
+   public static void main(String[] args) {
+      System.out.println("Hello, World");
+   }
 }
